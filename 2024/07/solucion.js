e40f8755ec1bc95f69d858c821616b1591dd0cb7
Luis Guerra <luis910gg@gmail.com>
2024-12-20 17:11:20 -0400

    Change solution 07

--- 2024/07/solucion.js
+++ 2024/07/solucion.js
@@ -5,7 +5,7 @@ function fixPackages(packages) {
   const regex = /\([^()]*\)/;
   if (!packages.match(regex)) return packages
   while (packages.match(regex)) {
-    let parenthesis = packages.match(regex)[0] ?? 0
+    let parenthesis = packages.match(regex)[0]
     const newText = parenthesis.slice(1, -1).split('').reverse().join('')
     packages = packages.replace(parenthesis, newText)
   }
